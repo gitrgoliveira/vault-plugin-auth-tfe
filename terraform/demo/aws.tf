ddc81d14dd9e91dc1a68d1b5efe601741b8e0206
Ricardo Oliveira <ricardo@hashicorp.com>
2021-03-02 16:57:00 +0000

    adding AWS demo.

---
+++ terraform/demo/aws.tf
@@ -0,0 +1,45 @@
+// Now fetching the AWS ephemeral credentials for some AWS work
+variable "region" { default = "eu-west-2" }
+variable "name" { default = "dynamic-aws-creds-operator" }
+variable "ttl" { default = "1" }
+
+data "vault_aws_access_credentials" "creds" {
+  backend = "aws"
+  role    = "deploy"
+  type    = "sts"
+}
+
+provider "aws" {
+  region     = var.region
+  access_key = data.vault_aws_access_credentials.creds.access_key
+  secret_key = data.vault_aws_access_credentials.creds.secret_key
+  token      = data.vault_aws_access_credentials.creds.security_token
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+# Create AWS EC2 Instance
+resource "aws_instance" "main" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.nano"
+
+  tags = {
+    Name  = var.name
+    TTL   = var.ttl
+    owner = "${var.name}-guide"
+  }
+}
