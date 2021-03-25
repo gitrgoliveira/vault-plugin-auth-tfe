7a3995ad5c622b4fb532307e783a2a18bcbd8448
Ricardo Oliveira <ricardo@hashicorp.com>
2021-03-25 16:56:54 +0000

    better demo

--- terraform/demo/variables.tf
+++ terraform/demo/variables.tf
@@ -1,10 +1,16 @@
 variable "TFC_WORKSPACE_NAME" {
   type        = string
-  description = "default variable defined in TFC/E environment"
+  description = "Workspace of the current run. This is read from the environment and should not be manually set."
 }
 
 variable "TFE_RUN_ID" {
   type        = string
-  description = "RunID of the current run"
+  description = "RunID of the current run. This is read from the environment and should not be manually set."
+}
+
+variable "VAULT_ADDR" {
+  type        = string
+  description = "Vault address"
+  default     = "http://88.97.2.109:8200"
 }
 
