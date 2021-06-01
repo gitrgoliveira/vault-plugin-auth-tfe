40c39c59d2394005c661c8a7e6a4ea7937eb5349
Ricardo Oliveira <ricardo@hashicorp.com>
2021-06-01 21:08:25 +0200

    making my demo file simpler.

--- terraform/demo/login_file.tf
+++ terraform/demo/login_file.tf
@@ -1,12 +1,6 @@
 #  helpers to figure out the TFC Agent file path
-locals {
-  file_path_apply = "/root/.tfc-agent/component/terraform/runs/${var.TFE_RUN_ID}.apply/cli.tfrc"
-  file_path_plan  = "/root/.tfc-agent/component/terraform/runs/${var.TFE_RUN_ID}.plan/cli.tfrc"
-  # file_path       = fileexists(local.file_path_apply) ? local.file_path_apply : local.file_path_plan
-}
-
 provider "vault" {
-  address    = "http://88.97.2.109:8200"
+  address    = "http://87.64.188.107:8200"
   token_name = "terraform-${var.TFE_RUN_ID}"
   auth_login {
     path = "auth/tfe-auth/login"
@@ -14,14 +8,9 @@ provider "vault" {
       role      = "workspace_role"
       workspace = var.TFC_WORKSPACE_NAME
       run-id    = var.TFE_RUN_ID
-      # For code that is running within TFC/TFE
-      tfe-credentials-file = filebase64("/tmp/cli.tfrc")
-
-      # For code that is using a TFC/Runner
-      # tfe-credentials-file = filebase64(local.file_path)
-
-      # For code to run in both
-      # tfe-credentials-file = fileexists("/tmp/cli.tfrc") ? filebase64("/tmp/cli.tfrc") : filebase64(local.file_path)
+      # For code that is running within TFC/TFE or using an external agent
+      tfe-credentials-file = try(filebase64("${path.cwd}/../.terraformrc"),
+                                  filebase64("/tmp/cli.tfrc"))
     }
   }
 }
